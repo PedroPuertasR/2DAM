59aab08510756469bef9984b9c1f98a5f6c6c120
alumno <alumno@pc10.iris.majuelo.es>
2022-10-06 17:00:13 +0200

    Ejercicios

---
+++ PSP/Ejercicios/Boletin1ProcesosPedroPuertas/ejercicio7/proceso7.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void main(){
+    pid_t hijo, padre, pid;
+
+    pid = fork();
+
+    if()
+}
