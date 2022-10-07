635c6786f1265d75d40059ff2d98eec205a18631
alumno <alumno@pc10.iris.majuelo.es>
2022-10-07 17:55:11 +0200

    Añadido ejercicios de ejemplo de herencias en C#

---
+++ DI/Ejercicios/Ejemplos2PedroPuertasAlbertoBlazquez/EjemploHerencia/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace EjemploHerencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public class Persona{
+
+            private string nombre;
+            private string apellidos;
+            private string dni;
+
+            public Persona(string nombre, string apellidos, string dni)
+            {
+                this.nombre = nombre;
+                this.apellidos = apellidos;
+                this.dni = dni;
+            }
+
+            public string Nombre 
+            { get 
+                { 
+                    return nombre; 
+                }
+              set
+                {
+                    this.nombre = value;
+                }
+            }
+
+            public string Apellidos
+            {
+                get 
+                {
+                    return apellidos; 
+                }
+
+                set 
+                {
+                    this.apellidos = value; 
+                }
+            }
+
+            public string Dni
+            {
+                get
+                {
+                    return dni;
+                }
+
+                set
+                {
+                    this.dni = value;
+                }
+            }
+
+            
+
+        }
+
+        public class Alumno : Persona
+        {
+
+            private double notaMedia;
+            private string curso;
+
+            public Alumno(double notaMedia, string curso) : base
+            {
+                this.notaMedia = notaMedia;
+                this.curso = curso;
+            }
+        }
+    }
+}
