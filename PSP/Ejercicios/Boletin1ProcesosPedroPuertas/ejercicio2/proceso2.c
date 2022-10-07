a203cd8ca79c073c9e2f46c6dcf63a529dbb6834
alumno <alumno@pc10.iris.majuelo.es>
2022-10-07 17:11:54 +0200

    Arreglado problema en proceso2.c y reemplazada captura

--- PSP/Ejercicios/Boletin1ProcesosPedroPuertas/ejercicio2/proceso2.c
+++ PSP/Ejercicios/Boletin1ProcesosPedroPuertas/ejercicio2/proceso2.c
@@ -4,5 +4,5 @@ void main(){
     printf("Ejemplo de uso de excel:\n");
     printf("Listado del directorio actual.");
     execl("/bin/ls", "ls", "-l", (char*)NULL);
-    printf("\n Esta instrucción no se llega a ejecutar.")
+    printf("\n Esta instrucción no se llega a ejecutar.");
 }
