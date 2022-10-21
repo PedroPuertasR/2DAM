4cf4b687cfca78c1d22126fbdf98dd3547375b27
alumno <alumno@pc10.iris.majuelo.es>
2022-10-21 17:49:30 +0200

    Ejercicios de PSP terminados hasta el 9

---
+++ PSP/Ejercicios/Boletin2ProcesosPedroPuertas/BoletinProcesosJava/src/ejercicio9/Principal.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author alumno
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String comando;
+        
+        if(System.getProperty("os.name").equalsIgnoreCase("linux")){
+
+            comando = "ls";
+            
+        }else{
+            comando = "cmd /c dir";
+        }
+        
+        for (int i = 0; i < args.length; i++) {
+            comando += args[i];
+        }
+        
+        Ejecuta e = new Ejecuta(comando);
+        
+    }
+    
+}
+
+class Ejecuta{
+    
+    public Ejecuta (String comando){
+        
+        InputStream is;
+        BufferedReader br;
+        String linea;
+        Runtime r = Runtime.getRuntime();
+        Process p;
+        
+        try{
+            
+            p = r.exec(comando);
+            p.waitFor();
+            
+            is = p.getInputStream();
+            
+            br = new BufferedReader(new InputStreamReader(is));
+            
+            while((linea = br.readLine()) != null){
+                System.out.println(linea);
+            }
+            
+        }catch(IOException e){
+            System.out.println("Error al crear el proceso.");
+            e.printStackTrace();
+        } catch (InterruptedException ex) {
+            System.out.println("Proceso interrumpido.");
+        }
+    }
+}
