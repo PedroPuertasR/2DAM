d1e2c979b76bd658e48b85ba930fdc4ee69de20a
alumno <alumno@pc10.iris.majuelo.es>
2022-10-24 21:13:52 +0200

    Añadidos ejemplos de pilas y listas en C#

---
+++ DI/Ejercicios/EjemploLista/Program.cs
@@ -0,0 +1,101 @@
+﻿namespace EjemploListas
+{
+    internal class Program
+    {
+        private delegate int ModificaEntero(int input);
+
+        public static void Main()
+        {
+            //Instanciamos el List
+
+            double lectura;
+            var lista = new List<double>();
+            
+            //Añadimos valores a la lista con el método Add.
+
+            lista.Add(7.2);
+            lista.Add(5.5);
+            lista.Add(6.3);
+            lista.Add(8);
+            lista.Add(4.5);
+            lista.Add(6.3);
+            lista.Add(9);
+
+            //Mostramos las "notas" de nuestra lista.
+
+            System.Console.WriteLine("Notas del curso: ");
+
+            foreach(double d in lista)
+            {
+                System.Console.WriteLine(d);
+            }
+
+            /* En caso de que queramos introducir un valor en una posición
+             * en particular podemos utilizar el método insert indicandole 
+             * la posición y el nuevo valor.
+             */
+
+            lista.Insert(1, 5);
+
+            System.Console.WriteLine("\nEjemplo del insert:");
+
+            System.Console.WriteLine(lista[2]);
+
+            /* A la hora de eliminar un elemento de la lista podemos hacerlo de
+             * varias formas. Eligiendo la posición en la que se encuentra el
+             * valor o introduciendo el valor directamente. 
+             */
+
+            lista.Remove(6.3);
+
+            System.Console.WriteLine("\nLista tras borrado:");
+
+            foreach (double d in lista)
+            {
+                System.Console.WriteLine(d);
+            }
+
+            /*En caso de que haya dos valores iguales dentro de una lista
+             *(Como es el caso anterior) solo se borrará uno de ellos.
+             */
+
+            /* También podemos buscar la posición en la que se encuentra un
+             * valor con el método indexOf
+             */
+            System.Console.WriteLine("\nIndique el número a buscar:");
+
+            lectura = System.Convert.ToDouble(System.Console.ReadLine());
+
+            if (lista.Contains(lectura))
+            {
+                System.Console.WriteLine("Se encuentra en la posición nº " + (lista.IndexOf(lectura) + 1));
+            }
+            else
+            {
+                System.Console.WriteLine("El número no se encuentra en la lista, seleccione otro:");
+                lectura = System.Convert.ToDouble(System.Console.ReadLine());
+            }
+
+            //Podemos comprobar el tamaño de la lista con el método Capacity.
+
+            System.Console.WriteLine("\nTamaño de la lista:");
+            System.Console.WriteLine(lista.Capacity);
+
+            //También podemos ordenar la lista
+
+            lista.Sort();
+
+            System.Console.WriteLine("\nLista ordenada:");
+
+            foreach (double d in lista)
+            {
+                System.Console.WriteLine(d);
+            }
+
+            //Por último podemos vaciar la lista con el método Clear
+
+            lista.Clear();
+
+        }
+    }
+}
---
+++ DI/Ejercicios/EjemploPila/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace EjemploStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciamos la pila.
+
+            var pila = new Stack<int>();
+
+            //Para introducir valores en la pila utilizamos el método push.
+
+            pila.Push(0);
+            pila.Push(1);
+            pila.Push(2);
+            pila.Push(3);
+            pila.Push(4);
+            pila.Push(5);
+
+            //Con el método count podemos sacar el tamaño de la pila.
+
+            System.Console.WriteLine("Tamaño: " + pila.Count);
+
+            /* Podemos comprobar el primer valor que contiene la
+            * pila con el método Peek.
+            */
+
+            System.Console.WriteLine("\nPrimer valor de la pila: " + pila.Peek());
+
+            /*Además con el método Contains comprobaremos si existe un número en 
+             * la pila.
+            */
+
+            System.Console.WriteLine("\nIndique el número que quiere buscar en la pila:");
+
+            int teclado = System.Convert.ToInt32(System.Console.ReadLine());
+
+            if (pila.Contains(teclado))
+            {
+                System.Console.WriteLine("\nSí se encuentra en la pila.");
+            }
+            else
+            {
+                System.Console.WriteLine("\nNo se encuentra en la pila.");
+            }
+
+            /*Para mostrar el contenido de la pila utilizamos el método Pop. Una vez
+             * que saquemos los valores de la pila esta quedará vacía.
+             */
+
+            System.Console.WriteLine("\nSalida de la pila:");
+
+            for (int i = 0; i < 6; i++)
+            {
+                System.Console.WriteLine(pila.Pop());
+            }
+
+            /* En una pila los últimos valores en entrar en ella son los 
+             * primeros valores en salir por eso en este ejemplo se hace
+             * una cuenta atras.
+             */
+
+            pila.Push(0);
+            pila.Push(1);
+            pila.Push(2);
+
+            //Por último podemos vaciar la pila con el método Clear
+
+            pila.Clear();
+
+        }
+    }
+}
