b69c18bd7268213eda1c020759ea91e1293ca536
alumno <alumno@pc10.iris.majuelo.es>
2022-11-18 17:52:42 +0100

    Ejercicios de BBDD boletín 3 cursores hecho hasta el 9

--- AD/Ejercicios/1 Trimestre/Cursores/boletinCursores3PedroPuertas.sql
+++ AD/Ejercicios/1 Trimestre/Cursores/boletinCursores3PedroPuertas.sql
@@ -90,30 +90,34 @@ end;
 create or replace procedure nuevo_emple(num_empl number, ape varchar2, ofi varchar2,
                                         dire number, fecha_cont date, sal number, comi number, dept number)
 as
-    cod_err number(6);
     vdept number;
-    vdir varchar2(10);
+    vdir number;
     vempl number;
     salario_nulo exception;
-    dept_err exception;
-    dir_err exception;
-    emp_err exception;
+    no_existe_departamento exception;
+    no_existe_director exception;
+    numero_empleado_duplicado exception;
 begin
+
+    if sal <= 0 then
+        raise_application_error(-20001, 'No puede insertarse un salario nulo.');
+    end if;
 
     select dept_no into vdept
     from depart
     where dept_no = dept;
 
     if vdept is null then
-        raise dept_err;
+        raise no_existe_departamento;
     end if;
 
-    select oficio into vdir
+    select emp_no into vdir
     from emple
-    where emp_no = dire;
+    where emp_no = dire
+          and oficio = 'DIRECTOR';
 
-    if vdir <> 'DIRECTOR' then
-        raise dir_err;
+    if vdir is null then
+        raise no_existe_director;
     end if;
 
     select emp_no into vempl
@@ -121,25 +125,242 @@ begin
     where emp_no = num_empl;
 
     if vempl = num_empl then
-        raise emp_err;
-    else
+        raise numero_empleado_duplicado;
+    elsif vempl is null then
+        dbms_output.put_line('emp');
         insert into emple values (num_empl, ape, ofi, dire, fecha_cont, sal, comi, dept);
     end if;
 
     dbms_output.put_line('Se ha insertado el nuevo empleado.');
 
     exception
-        when dept_err then
+        when no_existe_departamento then
             dbms_output.put_line('No existe el número de departamento.');
-        when dir_err then
+        when no_existe_director then
             dbms_output.put_line('No existe ningún director con ese número.');
-        when emp_err then
+        when numero_empleado_duplicado then
             dbms_output.put_line('El número de empleado no puede repetirse.');
         when too_many_rows then
             dbms_output.put_line('Se ha encontrado más de una fila en alguna consulta.');
         when others then
-            cod_err := SQLCODE;
-            dbms_output.put_line('Error número: ' || cod_err);
+            dbms_output.put_line(sqlcode || sqlerrm);
 end;
 
-exec nuevo_emple(7369, 'Puertas', 'ANALISTA', 7902, '06/01/2022', 25000, null, 10);
+exec nuevo_emple(1234, 'Puertas', 'ANALISTA', 7782, '06/01/2022', 25000, null, 10);
+
+/*Actividad propuesta 7*/
+
+/*Ejercicio complementario 7*/
+
+create or replace procedure subir_sueldo_todos
+as
+    cursor c1 is select distinct oficio 
+                 from emple;
+    cursor c2(ofi varchar2) is select avg(salario) as media
+                               from emple
+                               where oficio = ofi;
+    cursor c3(ofi varchar2) is select emp_no
+                               from emple
+                               where oficio = ofi;
+    subida number := 0;
+    vsal number;
+    tot_filas number := 0;
+begin
+    commit;
+
+    for v1 in c1 loop
+
+        for v2 in c2(v1.oficio) loop
+
+            for v3 in c3(v1.oficio) loop
+
+                select salario into vsal
+                from emple
+                where emp_no = v3.emp_no;
+
+                if vsal < v2.media then
+                    subida := (v2.media - vsal) * 0.5;
+
+                    update emple set salario = salario + subida where emp_no = v3.emp_no;
+
+                    tot_filas := tot_filas + 1;
+
+                    subida := 0;
+                end if;
+            end loop;
+        end loop;
+    end loop;
+
+    dbms_output.put_line('Total de filas afectadas: ' || tot_filas);
+
+    commit;
+
+    exception
+        when too_many_rows then
+            rollback;
+            dbms_output.put_line('Demasiadas columnas en el salario.');
+        when no_data_found then
+            rollback;
+            dbms_output.put_line('No se ha encontrado el salario.');
+        when others then
+            rollback;
+            dbms_output.put_line(sqlcode || sqlerrm);
+end;
+
+/*Ejercicio complementario 8*/
+
+create or replace procedure liquidacion_emple
+as
+    cursor c1 is select *
+              from emple
+              order by apellido;
+    total number := 0;
+    trienio number := 0;
+    anios number := 0;
+    resp number := 0;
+    sum_resp number;
+    comi number;
+    contador number := 0;
+begin
+
+    for v1 in c1 loop
+
+        dbms_output.put_line('***********************************');
+        dbms_output.put_line('Liquidación del empleado: ' || v1.apellido);
+        dbms_output.put_line('Departamento: ' || v1.dept_no);
+        dbms_output.put_line('Oficio: ' || v1.oficio);
+        dbms_output.put_line('Salario: ' || v1.salario || '€');
+
+        select floor(months_between(sysdate, v1.fecha_alt) / 12) / 3 into anios
+        from dual;
+
+        while contador < anios loop
+            trienio := trienio + 50;
+        end loop;
+
+        contador := 0;
+
+        dbms_output.put_line('Trienios: ' || trienio || '€');
+
+        select count(*) into resp
+        from emple
+        where dir = v1.emp_no;
+
+        while contador < resp loop
+            sum_resp := sum_resp + 100;
+        end loop;
+
+        contador := 0;
+
+        dbms_output.put_line('Comp. responsabilidad: ' || sum_resp);
+
+        if v1.comision is null then
+            comi := 0;
+        else
+            comi := v1.comision;
+        end if;
+
+        dbms_output.put_line('Comisión: ' || comi || '€');
+
+        total := v1.salario + trienio + comi + sum_resp;
+
+        dbms_output.put_line('***********************************');
+        dbms_output.put_line(total);
+        dbms_output.put_line('***********************************');
+
+        trienio := 0;
+        comi := 0;
+        sum_resp := 0;
+
+    end loop;
+
+    exception
+        when too_many_rows then
+            dbms_output.put_line('Demasiadas filas en alguna consulta.');
+        when no_data_found then
+            dbms_output.put_line('No se han encontrado datos en una consulta.');
+        when others then
+            dbms_output.put_line(sqlcode || sqlerrm);
+end;
+
+/*Ejercicio complementario 9*/
+
+create table t_liquidacion(
+    emp_no number primary key,
+    apellido varchar2,
+    departamento number,
+    oficio varchar2,
+    salario number, 
+    trienios number,
+    comp_responsabilidad number,
+    comision number,
+    total number
+);
+
+create or replace procedure liquidacion_emple
+as
+    cursor c1 is select *
+              from emple
+              order by apellido;
+    total number := 0;
+    trienio number := 0;
+    anios number := 0;
+    resp number := 0;
+    sum_resp number;
+    comi number;
+    contador number := 0;
+begin
+
+    commit;
+
+    for v1 in c1 loop
+
+        select floor(months_between(sysdate, v1.fecha_alt) / 12) / 3 into anios
+        from dual;
+
+        while contador < anios loop
+            trienio := trienio + 50;
+        end loop;
+
+        contador := 0;
+
+        select count(*) into resp
+        from emple
+        where dir = v1.emp_no;
+
+        while contador < resp loop
+            sum_resp := sum_resp + 100;
+        end loop;
+
+        contador := 0;
+
+        if v1.comision is null then
+            comi := 0;
+        else
+            comi := v1.comision;
+        end if;
+
+        total := v1.salario + trienio + comi + sum_resp;
+
+        trienio := 0;
+        comi := 0;
+        sum_resp := 0;
+
+        insert into t_liquidacion values (v1.emp_no, v1.apellido, v1.dept_no, v1.oficio, v1.salario, trienio, sum_resp, comi, total);
+
+    end loop;
+
+    exception
+        when too_many_rows then
+            dbms_output.put_line('Demasiadas filas en alguna consulta.');
+            rollback;
+        when no_data_found then
+            dbms_output.put_line('No se han encontrado datos en una consulta.');
+            rollback;
+        when others then
+            dbms_output.put_line(sqlcode || sqlerrm);
+            rollback;
+end;
+
+/*Ejercicio complementario 10*/
+
