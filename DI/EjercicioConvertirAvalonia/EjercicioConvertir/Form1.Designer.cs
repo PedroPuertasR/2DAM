8916200fdc9482b81bfdff485e17f453f8e46e5d
alumno <alumno@pc10.iris.majuelo.es>
2022-12-02 15:16:11 +0100

    Ejercicio de avalonia terminado

---
+++ DI/EjercicioConvertirAvalonia/EjercicioConvertir/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace EjercicioConvertir
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnArchivo = new System.Windows.Forms.Button();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbArchivo = new System.Windows.Forms.TextBox();
+            this.ofArchivo = new System.Windows.Forms.OpenFileDialog();
+            this.panelSobre = new System.Windows.Forms.Panel();
+            this.lblSobre = new System.Windows.Forms.Label();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.btnSi = new System.Windows.Forms.Button();
+            this.cbTipo = new System.Windows.Forms.ComboBox();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.panelSobre.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnArchivo
+            // 
+            this.btnArchivo.Location = new System.Drawing.Point(74, 138);
+            this.btnArchivo.Name = "btnArchivo";
+            this.btnArchivo.Size = new System.Drawing.Size(137, 23);
+            this.btnArchivo.TabIndex = 0;
+            this.btnArchivo.Text = "Seleccionar archivo";
+            this.btnArchivo.UseVisualStyleBackColor = true;
+            this.btnArchivo.Click += new System.EventHandler(this.btnArchivo_Click);
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(570, 76);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(110, 42);
+            this.btnConvertir.TabIndex = 1;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(108, 90);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(103, 15);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "Nombre de salida:";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(246, 87);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(148, 23);
+            this.tbNombre.TabIndex = 4;
+            // 
+            // tbArchivo
+            // 
+            this.tbArchivo.Location = new System.Drawing.Point(246, 139);
+            this.tbArchivo.Name = "tbArchivo";
+            this.tbArchivo.Size = new System.Drawing.Size(148, 23);
+            this.tbArchivo.TabIndex = 5;
+            this.tbArchivo.Visible = false;
+            // 
+            // ofArchivo
+            // 
+            this.ofArchivo.FileName = "openFileDialog1";
+            // 
+            // panelSobre
+            // 
+            this.panelSobre.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panelSobre.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelSobre.Controls.Add(this.lblSobre);
+            this.panelSobre.Controls.Add(this.btnNo);
+            this.panelSobre.Controls.Add(this.btnSi);
+            this.panelSobre.Location = new System.Drawing.Point(322, 190);
+            this.panelSobre.Name = "panelSobre";
+            this.panelSobre.Size = new System.Drawing.Size(261, 104);
+            this.panelSobre.TabIndex = 7;
+            this.panelSobre.Visible = false;
+            // 
+            // lblSobre
+            // 
+            this.lblSobre.AutoSize = true;
+            this.lblSobre.Location = new System.Drawing.Point(35, 16);
+            this.lblSobre.Name = "lblSobre";
+            this.lblSobre.Size = new System.Drawing.Size(32, 15);
+            this.lblSobre.TabIndex = 2;
+            this.lblSobre.Text = "label";
+            // 
+            // btnNo
+            // 
+            this.btnNo.Location = new System.Drawing.Point(182, 49);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(36, 23);
+            this.btnNo.TabIndex = 1;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // btnSi
+            // 
+            this.btnSi.Location = new System.Drawing.Point(29, 49);
+            this.btnSi.Name = "btnSi";
+            this.btnSi.Size = new System.Drawing.Size(38, 23);
+            this.btnSi.TabIndex = 0;
+            this.btnSi.Text = "Sí";
+            this.btnSi.UseVisualStyleBackColor = true;
+            this.btnSi.Click += new System.EventHandler(this.btnSi_Click);
+            // 
+            // cbTipo
+            // 
+            this.cbTipo.FormattingEnabled = true;
+            this.cbTipo.Items.AddRange(new object[] {
+            "wav",
+            "mp3"});
+            this.cbTipo.Location = new System.Drawing.Point(246, 34);
+            this.cbTipo.Name = "cbTipo";
+            this.cbTipo.Size = new System.Drawing.Size(149, 23);
+            this.cbTipo.TabIndex = 8;
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(119, 37);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(92, 15);
+            this.lblTipo.TabIndex = 9;
+            this.lblTipo.Text = "Tipo de archivo:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 317);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.cbTipo);
+            this.Controls.Add(this.panelSobre);
+            this.Controls.Add(this.tbArchivo);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.btnArchivo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelSobre.ResumeLayout(false);
+            this.panelSobre.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnArchivo;
+        private Button btnConvertir;
+        private Label lblNombre;
+        private TextBox tbNombre;
+        private TextBox tbArchivo;
+        private OpenFileDialog ofArchivo;
+        private Panel panelSobre;
+        private Label lblSobre;
+        private Button btnNo;
+        private Button btnSi;
+        private ComboBox cbTipo;
+        private Label lblTipo;
+    }
+}
---
+++ DI/EjercicioConvertirAvalonia/EjercicioConvertir/Form1.cs
@@ -0,0 +1,133 @@
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace EjercicioConvertir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*En caso de que pulsemos el bot�n de seleccionar archivo se mostrar� el openFileDialog para que el
+         * usuario pueda seleccionarlo
+         */
+        private void btnArchivo_Click(object sender, EventArgs e)
+        {
+            ofArchivo.ShowDialog();
+
+            //Guardamos en una variable el nombre y la extensi�n del archivo
+            tbArchivo.Text = ofArchivo.SafeFileName;
+
+            //Solo si el archivo existe mostrar� en un textBox el nombre y su extensi�n
+            if (ofArchivo.CheckFileExists)
+            {
+                tbArchivo.Visible = true;
+            }
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            //En caso de que no seleccione un archivo le avisa y no se hace nada.
+            if (tbArchivo.Text.Equals(""))
+            {
+                //Muestra un messageBox al usuario
+                MessageBox.Show("Seleccione un archivo");
+            }
+            else
+            {
+                //En caso de que no seleccione un nombre del nuevo archivo le avisa y no se hace nada.
+                if (tbNombre.Text.Equals(""))
+                {
+                    //Muestra un messageBox al usuario
+                    MessageBox.Show("Elija un nombre");
+                }
+                else
+                {
+                    //En caso de que no seleccione un tipo al que convertir le avisa y no se hace nada.
+                    if (cbTipo.Text.Equals(""))
+                    {
+                        //Muestra un messageBox al usuario
+                        MessageBox.Show("Seleccione un tipo al que convertir");
+                    }
+                    else
+                    {
+                        //Guardamos la ruta del openFileDialog
+                        string ruta = ofArchivo.FileName;
+
+                        //Instanciamos la variable que ejecutar� el proceso
+                        var ps = new ProcessStartInfo();
+
+                        //El proceso ejecutar� el cmd
+                        ps.FileName = "cmd.exe";
+
+                        //No se abrir� la consola en el proceso gracias a este m�todo
+                        ps.UseShellExecute = false;
+
+                        /*Le pasamos las instrucciones con la ruta y el nombre de archivo 
+                         * para que realice la conversi�n
+                         */
+                        ps.Arguments = "/c ffmpeg -i " + ruta + " " + Path.GetDirectoryName(ruta) + "\\" + tbNombre.Text + "." + cbTipo.Text;
+
+                        /*Guardamos en un string la ruta del archivo con el nuevo nombre 
+                         * por si coincide con uno que ya exista
+                         */
+                        string sobre = Path.GetDirectoryName(ruta) + "\\" + tbNombre.Text + "." + cbTipo.Text;
+
+                        /*Si nuestra variable coincide con el nuevo archivo ponemos como visible un panel que
+                         * le preguntar� al usuario si desea sobreescribir el archivo
+                         */
+                        if (File.Exists(sobre))
+                        {
+                            panelSobre.Visible = true;
+
+                            lblSobre.Text = "El archivo ya existe\n¿Desea sobreescribirlo?";
+                        }
+                        /*En caso de que no exista realizar� el proceso normalmente y mostrar� un 
+                         * label de �xito en la conversi�n
+                         */
+                        else
+                        {
+                            //Iniciamos el proceso para realizar la conversi�n
+                            using var proceso = Process.Start(ps);
+
+                            //Cuando se ejecute el proceso mostrar� un MessageBox de �xito
+                            MessageBox.Show("Conversión a ." + cbTipo.Text + " realizada.");
+                        }
+                    }
+                }
+            }
+        }
+
+        /*En caso de que el usuario elija que no en la sobreescritura simplemente ocultar� el panel 
+         * y no har� nada
+         */
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            panelSobre.Visible = false;
+        }
+
+        /*En caso de que el usuario seleccione que si se ejecutar� el comando del cmd con un -y delante, 
+         * el cual har� que se sobreescriba autom�ticamente sin preguntar nada al usuario por consola
+         */
+        private void btnSi_Click(object sender, EventArgs e)
+        {
+            //Oculta el panel una vez que pulsemos el s�
+            panelSobre.Visible = false;
+            string ruta = ofArchivo.FileName;
+            var ps = new ProcessStartInfo();
+
+            //Instrucciones a la hora de ejecutar el proceso
+            ps.FileName = "cmd.exe";
+            ps.UseShellExecute = false;
+            ps.Arguments = "/c ffmpeg -y -i " + ruta + " " + Path.GetDirectoryName(ruta) + "\\" + tbNombre.Text + "." + cbTipo.Text;
+
+            //Iniciamos el proceso para que convierta el archivo
+            using var proceso = Process.Start(ps);
+
+            //Cuando se ejecute el proceso mostrar� un MessageBox de �xito
+            MessageBox.Show("Conversión a ." + cbTipo.Text + " realizada.");
+        }
+    }
+}
