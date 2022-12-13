e162b6b232dd8f592b05d48f31f19f2eafe2d6de
alumno <alumno@pc10.iris.majuelo.es>
2022-12-13 21:19:25 +0100

    Cambiadas coordenadas

--- SGE/python/ejercicios/Videojuego/JuegoPedroPuertas/main.py
+++ SGE/python/ejercicios/Videojuego/JuegoPedroPuertas/main.py
@@ -4,7 +4,7 @@ from pelota import Pelota
 
 
 black = (0, 0, 0)
-white = (255, 255, 255)
+
 dimen = (20, 100)
 dimen_pelota = (40, 40)
 
@@ -21,10 +21,15 @@ jugador2_teclaDOWN = pygame.K_DOWN
 
 jugador_y = 250
 jugador2_y = 250
+jugador_x = 20
+jugador2_x = 780
 
-jugador = Jugador(20, jugador_y, dimen)
-jugador2 = Jugador(780, jugador2_y, dimen)
-pelota = Pelota(400, 300, dimen_pelota)
+pelota_x = 400
+pelota_y = 300
+
+jugador = Jugador(jugador_x, jugador_y, dimen)
+jugador2 = Jugador(jugador2_x, jugador2_y, dimen)
+pelota = Pelota(pelota_x, pelota_y, dimen_pelota)
 
 
 lista_sprites = pygame.sprite.Group()
