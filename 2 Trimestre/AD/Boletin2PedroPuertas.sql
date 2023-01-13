ca83b1c5a2ffad6918b8ba840d9953dfef39821b
alumno <alumno@pc10.iris.majuelo.es>
2023-01-13 21:21:58 +0100

    AD casi terminado

---
+++ 2 Trimestre/AD/Boletin2PedroPuertas.sql
@@ -0,0 +1,139 @@
+-- Ejercicio 1
+
+create or replace trigger t1
+after delete on depart
+for each row
+begin
+    delete from emple where dept_no = :old.dept_no;
+end;
+
+-- Ejercicio 2
+
+create or replace trigger t2
+after update on depart
+for each row
+begin
+    update emple set dept_no = :new.dept_no where dept_no = :old.dept_no;
+end;
+
+-- Ejercicio 3
+
+create or replace trigger t3
+before insert or update on emple
+for each row
+declare
+    v1 integer;
+begin
+    select count(*) into v1
+    from depart
+    where dept_no = :new.dept_no;
+
+    if v1 = 0 then
+        raise_application_error(-20000, 'El departamento no existe.');
+    end if;
+end;
+
+-- Ejercicio 4
+
+create or replace trigger t4
+before update on emple
+for each row
+begin
+    declare
+        vsal number;
+    begin
+        select salario * 1.2 into vsal
+        from emple
+        where emp_no = :new.emp_no;
+
+        if :new.salario > vsal then
+            raise_application_error(-20000, 'No se puede aumentar el salario en más de un 20%.');
+        end if;
+    end;
+end;
+
+-- Ejercicio 5
+
+create or replace trigger t5
+before update on emple
+for each row
+declare
+    vsal number;
+begin
+    if updating ('apellido') or updating ('emp_no') then
+        raise_application_error(-20000, 'no se puede modificar el apellido o número de empleado.');
+    end if;
+
+    select salario * 1.2 into vsal 
+    from emple
+    where emp_no = :new.emp_no;
+
+    if :new.salario > vsal then
+        raise_application_error(-20000, 'no se puede aumentar el salario en más de un 20%.');
+    end if;
+end;
+
+-- Ejercicio 6
+
+create or replace trigger t6
+before insert on emple
+for each row
+begin
+    new.comision := :new.salario * 0.01;
+end;
+
+-- Ejercicio 7
+
+create or replace trigger t7
+after insert or delete on personal
+for each row
+begin
+    if inserting then
+        if :new.funcion = 'profesor' then
+            insert into profesores values (:new.cod_centro, :new.dni, :new.apellidos, :new.funcion);
+        end if;
+
+    elsif deleting then
+        if :old.funcion = 'profesor' then
+            delete from profesores where cod_centro = :old.cod_centro and dni = :old.dni;
+        end if;
+    end if;
+end;
+
+-- Ejercicio 8
+
+create table libros (
+    isbn       varchar2(13) primary key,
+    genero     varchar2(20) not null,
+    titulo     varchar2(50) not null,
+    autor      varchar2(50) not null
+);
+
+create table estadisticas (
+    genero     varchar2(20) primary key,
+    total_libros number(10) not null
+);
+
+create or replace trigger trg_update_estadisticas
+after insert or update or delete on libros
+for each row
+declare
+    num_libros number;
+begin
+    if inserting then
+
+        select count(*) into num_libros from libros where genero = :new.genero;
+        insert into estadisticas (genero, total_libros) values (:new.genero, num_libros);
+
+    elsif updating then
+
+        select count(*) into num_libros from libros where genero = :new.genero;
+        update estadisticas set total_libros = num_libros where genero = :new.genero;
+
+    elsif deleting then
+
+        select count(*) into num_libros from libros where genero = :old.genero;
+        update estadisticas set total_libros = num_libros where genero = :old.genero;
+
+    end if;
+end;
--- 2 Trimestre/AD/Boletín1PedroPuertas.sql
+++ 2 Trimestre/AD/Boletín1PedroPuertas.sql
@@ -67,6 +67,10 @@ columna modificada (sólo en las columnas modificadas).
 
 --Actividad complementaria 2
 
+create table auditaremple (
+    col1 varchar2(200) primary key
+);
+
 create or replace trigger t3
 after update on emple
 for each row
@@ -75,41 +79,70 @@ declare
 begin
     dbms_output.put_line('Se ha modificado al empleado nº' || :old.emp_no);
 
-    v1 := 'Modificación: ';
+    v1 := 'Modificación: ' || chr(10);
+    
+    if updating('emp_no') then
 
-    if :old.emp_no <> :new.emp_no then
+        v1 := v1 || 'Antigüo nº' || :old.emp_no || ', nuevo nº' || :new.emp_no || '.' || chr(10);
 
-        v1 := v1 + 'Antigüo nº' || :old.emp_no || ', nuevo nº' || :new.emp_no || '.' || chr(10);
+    end if;
+    if updating('apellido') then
 
-    elif :old.apellido <> :new.emp_no then
-
-        v1 := v1 + 'Antigüo apellido: ' || :old.apellido || ', nuevo apellido: ' || :new.apellido || '.' || chr(10);
-
-    elif :old.oficio <> :new.oficio then
-
-        v1 := v1 + 'Antigüo oficio: ' || :old.oficio || ', nuevo oficio: ' || :new.oficio || '.' || chr(10);
-
-    elif :old.dir <> :new.dir then
-
-        v1 := v1 + 'Antigüo jefe: ' || :old.dir || ', nuevo jefe: ' || :new.dir || '.' || chr(10);
-
-    elif :old.fecha_alt <> :new.fecha_alt then
-
-        v1 := v1 + 'Antigüa fecha: ' || :old.fecha_alt || ', nueva fecha: ' || :new.fecha_alt || '.' || chr(10);
-
-    elif :old.salario <> :new.salario then
-
-        v1 := v1 + 'Antigüo salario: ' || :old.salario || '€, nuevo salario: ' || :new.salario || '€.' || chr(10);
-
-    elif :old.comision <> :new.comision then
-
-        v1 := v1 + 'Antigüa comisión: ' || :old.comision || '€, nueva comisión: ' || :new.comision || '€.' || chr(10);
-
-    elif :old.dept_no <> :new.dept_no then
-
-        v1 := v1 + 'Antigüo dept: ' || :old.dept_no || ', nuevo dept: ' || :new_dept_no || '.' || chr(10);
+        v1 := v1 || 'Antigüo apellido: ' || :old.apellido || ', nuevo apellido: ' || :new.apellido || '.' || chr(10);
 
     end if;
 
-    insert into auditaremple values ('Fecha: ' || sysdate || ' ' || to_char(sysdate, 'HH24:MI:SS') || 'Nº: ' || :old.emp_no || ', apellido: ' || :old.apellido || ', ' || v1);
+    if updating('oficio') then
+
+        v1 := v1 || 'Antigüo oficio: ' || :old.oficio || ', nuevo oficio: ' || :new.oficio || '.' || chr(10);
+
+    end if;
+
+    if updating('dir') then
+
+        v1 := v1 || 'Antigüo jefe: ' || :old.dir || ', nuevo jefe: ' || :new.dir || '.' || chr(10);
+
+    end if;
+
+    if updating('fecha_alt') then
+
+        v1 := v1 || 'Antigüa fecha: ' || :old.fecha_alt || ', nueva fecha: ' || :new.fecha_alt || '.' || chr(10);
+
+    end if;
+
+    if updating('salario') then
+
+        v1 := v1 || 'Antigüo salario: ' || :old.salario || '€, nuevo salario: ' || :new.salario || '€.' || chr(10);
+
+    end if;
+
+    if updating('comision') then
+
+        if :old.comision is NULL then
+
+            v1 := v1 || 'Antigüa comisión: 0€, nueva comisión: ' || :new.comision || '€.' || chr(10);
+
+        else
+
+            if :new.comision is NULL then
+
+                v1 := v1 || 'Antigüa comisión: ' || :old.comision || '€, nueva comisión: 0€.' || chr(10);
+
+            else
+
+                v1 := v1 || 'Antigüa comisión: ' || :old.comision || '€, nueva comisión: ' || :new.comision || '€.' || chr(10);
+
+            end if;
+
+        end if;
+
+    end if;
+
+    if updating('dept_no') then
+
+        v1 := v1 || 'Antigüo dept: ' || :old.dept_no || ', nuevo dept: ' || :new.dept_no || '.' || chr(10);
+
+    end if;
+
+    insert into auditaremple values ('Fecha: ' || sysdate || ' ' || to_char(sysdate, 'HH24:MI:SS') || ', Nº: ' || :old.emp_no || ', apellido: ' || :old.apellido || ', ' || v1);
 end;
