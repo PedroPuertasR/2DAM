c551a6f7d53692d4950b0009e31bbff923186a78
alumno <alumno@pc10.iris.majuelo.es>
2023-01-25 15:24:02 +0100

    Añadidos comentarios al proyecto

--- 2 Trimestre/DI/EjemploAutomatizacion/Program.cs
+++ 2 Trimestre/DI/EjemploAutomatizacion/Program.cs
@@ -22,6 +22,10 @@ namespace EjemploAutomatizacion
 
                     if (ventana != null)
                     {
+                        /* A través de las variables hijo iremos encontrando los menús de la aplicación
+                         * notepad.exe , tendremos que movernos entre los hijos hasta encontrar el
+                         * menú de ayuda.
+                         */
                         var hijo = ventana.FindAllChildren();
 
                         hijo[3].Click();
@@ -33,23 +37,34 @@ namespace EjemploAutomatizacion
                         var hijo3 = hijo2[4].FindAllChildren();
                         var hijo4 = hijo3[0].FindAllChildren();
 
+                        //Haciendo click en esta posición del hijo4 abriremos el panel Acerca de...
                         hijo4[2].Click();
 
-                        Thread.Sleep(1000);
+                        //Esperamos un segundo para realizar la captura
+                        Thread.Sleep(3000);
 
-                        Keyboard.Type("Frase celebre: " + texto);
-
-                        Thread.Sleep(1000);
-
+                        //Con la clase Capture realizaremos una foto de la pantalla principal
                         var captura = Capture.MainScreen();
+                        //La guardamos con el nombre captura.png
                         captura.ToFile("captura.png");
 
+                        //Justo después escribimos la frase celebre a través del método type de Keyboard
+                        Keyboard.Type("Frase celebre: " + texto);
+
+                        //Esperamos 3 segundos para guardar el texto y salir de la aplicación
+                        Thread.Sleep(3000);
+
+                        /* Realizamos las combinaciones de teclas que nos permitan guardar el archivo y
+                         * salir. En nuestro caso un Alt + F4, escritura del nombre, ENTER para guardar
+                         * y en caso de que tengamos que sobreescribir TAB + ENTER para pulsar en Sí
+                         */
                         Keyboard.Press(FlaUI.Core.WindowsAPI.VirtualKeyShort.ALT);
                         Keyboard.Press(FlaUI.Core.WindowsAPI.VirtualKeyShort.F4);
                         Keyboard.Release(FlaUI.Core.WindowsAPI.VirtualKeyShort.ALT);
                         Keyboard.Release(FlaUI.Core.WindowsAPI.VirtualKeyShort.F4);
                         Keyboard.Press(FlaUI.Core.WindowsAPI.VirtualKeyShort.ENTER);
                         Keyboard.Release(FlaUI.Core.WindowsAPI.VirtualKeyShort.ENTER);
+                        //Indicamos el nombre del archivo
                         Keyboard.Type("texto.txt");
                         Keyboard.Press(FlaUI.Core.WindowsAPI.VirtualKeyShort.ENTER);
                         Keyboard.Release(FlaUI.Core.WindowsAPI.VirtualKeyShort.ENTER);
@@ -60,6 +75,7 @@ namespace EjemploAutomatizacion
 
                         Thread.Sleep(3000);
 
+                        //Cerramos la aplicación
                         app.Close();
 
                     }
