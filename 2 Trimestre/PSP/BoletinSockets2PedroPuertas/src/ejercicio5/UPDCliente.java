a9f5d1ef43ea33af161a9b0adaed0b321429ded6
PedroPuertasR <92780740+PedroPuertasR@users.noreply.github.com>
2023-02-01 18:00:44 +0100

    Ejercicios del boletín terminado

---
+++ 2 Trimestre/PSP/BoletinSockets2PedroPuertas/src/ejercicio5/UPDCliente.java
@@ -0,0 +1,25 @@
+package ejercicio5;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UPDCliente {
+    public static void main(String[] args) throws IOException {
+        int port = 12345;
+        InetAddress address = InetAddress.getByName("192.168.8.9");
+        byte[] msg = ("Pedro Puertas Rodríguez").getBytes();
+        
+        DatagramPacket send = new DatagramPacket(msg, msg.length, address, port);
+        DatagramSocket socket = new DatagramSocket(34567);
+        socket.connect(address, port);
+        
+        System.out.println("Destino:            " + address.getHostAddress());
+        System.out.println("Puerto local:       " + socket.getLocalPort());
+        System.out.println("Puerto de destino:  " + socket.getPort());
+        
+        socket.send(send);
+        socket.close();
+    }
+}
---
+++ 2 Trimestre/PSP/BoletinSockets2PedroPuertas/src/ejercicio7/MCServer.java
@@ -0,0 +1,40 @@
+package ejercicio7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class MCServer {
+
+    public static void main(String[] args) throws IOException {
+        int port = 12345;
+        InetAddress mcGroup = InetAddress.getByName("225.0.0.1");
+        DatagramPacket send = new DatagramPacket(new byte[0], 0, mcGroup, port);
+        MulticastSocket socket = new MulticastSocket();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        String aux;
+        byte[] msg;
+
+        do {
+            System.out.println("Introduce un mensaje para enviarlo al servidor:\n");
+            aux = reader.readLine();
+            msg = aux.getBytes();
+
+            if ("*".equals(aux)) {
+                break;
+            }
+
+            send.setData(msg, 0, msg.length);
+            socket.send(send);
+        } while (true);
+
+        socket.close();
+
+    }
+
+}
