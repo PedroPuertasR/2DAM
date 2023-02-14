86854ae86bd20d07b847cd0d20afb49e3394d9ae
pedropuertasr <pedropuertasr@gmail.com>
2023-02-14 14:09:11 +0100

    Juego hecho

--- 2 Trimestre/PMDM/HLC3/www/juego.php
+++ 2 Trimestre/PMDM/HLC3/www/juego.php
@@ -1,7 +1,7 @@
 <?php  
 include 'conexionbd.php';
 session_start();
-//isset — Determina si una variable está definida y no es null
+
 if (isset($_SESSION['masControl']))
 {
 	$con=conexion();
@@ -12,16 +12,18 @@ if (isset($_SESSION['masControl']))
 else
 {
 	session_destroy();
-	header("location:./verDatos.php");
+	header("location:./index.php");
 }
 ?>
-
-
 <html>
 <head>
     <title>Adivina el número</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
+    <div class="d-flex justify-content-center align-items-center">
+
     <?php
 
     $sumar = 0;
@@ -33,7 +35,7 @@ else
         echo "<script>console.log('Variable: ', ".json_encode($numero_a_adivinar).");</script>";
         $limite_inferior = 1;
         $limite_superior = 100;
-        $intentos = 0;
+        $intentos = 1;
     } else {
         $numero_a_adivinar = $_POST["numero_a_adivinar"];
         echo "<script>console.log('Variable: ', ".json_encode($numero_a_adivinar).");</script>";
@@ -44,10 +46,16 @@ else
         $respuesta = (int) $_POST["respuesta"];
 
         if ($respuesta == $numero_a_adivinar) {
-            echo "<p>¡Felicidades, has adivinado el número en $intentos intentos!</p>";
-            echo '<form action="juego.php" method="post">';
+            echo '<p>¡Felicidades, has adivinado el número en ' . $intentos . ' intentos!</p>';
+            echo '<div class="d-flex justify-content-center"><form action="juego.php" method="post">';
             echo '<input type="submit" name="reset" value="Jugar de nuevo">';
             echo '</form>';
+
+            $con=conexion();
+            $sql="UPDATE USUARIOS SET INTENTOS = " . $intentos . ", FECHA_RECORD = '" . $fecha_actual . "' WHERE EMAIL='" . $_SESSION['email'] . "';";
+            $resultado=mysqli_query($con, $sql);
+            mysqli_close($con);
+
             exit;
         } else{
             echo "<p>Número incorrecto</p>";
@@ -66,6 +74,8 @@ else
         $intentos++;
     }
     ?>
+
+    </div>
     <form action="juego.php" method="post">
         <input type="hidden" name="numero_a_adivinar" value="<?php echo $numero_a_adivinar; ?>">
         <input type="hidden" name="limite_inferior" value="<?php echo $limite_inferior; ?>">
