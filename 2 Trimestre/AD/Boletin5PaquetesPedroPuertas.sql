1e4cd67ce4159130e7a49f52fc623c1463279713
pedropuertasr <pedropuertasr@gmail.com>
2023-02-17 12:20:47 +0100

    Ejercicios de AD terminados

--- 2 Trimestre/AD/Boletin5PaquetesPedroPuertas.sql
+++ 2 Trimestre/AD/Boletin5PaquetesPedroPuertas.sql
@@ -1,25 +1,25 @@
 -- Ejercicio 1
 
 create or replace package pk1 as
-    type t_emple is table of emple.dept_no%type index by BINARY_INTEGER;
+    type t_emple is table of emple.emp_no%type index by BINARY_INTEGER;
     tipo1 t_emple;
 end;
 
 create or replace trigger t1
-before insert or update or delete on emple
+before insert or update on emple
 begin
     pk1.tipo1.delete;
 end;
 
 create or replace trigger t2
-after insert or update or delete on emple
+after insert or update on emple
 for each row
 begin
-    pk1.tipo1(:new.dept_no) := :new.dept_no;
+    pk1.tipo1(:new.emp_no) := :new.dept_no;
 end;
 
 create or replace trigger t3
-after insert or update or delete on emple
+after insert or update on emple
 declare
     cantidad number;
     i number;
@@ -47,102 +47,187 @@ update emple set dept_no = 30 where emp_no = 7566;
 
 -- Ejercicio 2
 
+create or replace package pk1 as
+    type t_emple is table of emple.dept_no%type index by BINARY_INTEGER;
+    tipo1 t_emple;
+end;
+
 create or replace trigger t1
+before insert or update on emple
+begin
+    pk1.tipo1.delete;
+end;
+
+create or replace trigger t2
+after insert or update on emple
+for each row
+begin
+    pk1.tipo1(:new.emp_no) := :new.dept_no;
+end;
+
+create or replace trigger t3
 after insert or update on emple
 declare
-    vtot number;
-    cursor c1 is select * from depart;
+    i number;
+    limite number;
 begin
+    i := pk1.tipo1.first;
 
-    for v1 in c1 loop
-    
-        select sum(salario) into vtot
+    while i is not null loop
+
+        select sum(salario) into limite
         from emple
-        where dept_no = v1.dept_no;
+        where dept_no = pk1.tipo1(i);
 
-        if vtot > 150000 then
-            raise_application_error(-20002, 'El salario no puede ser mayor que 150.000€');
+        if limite > 150000 then
+            raise_application_error(-20002, 'El total de salario del departamento no puede ser mayor que 150000.');
         end if;
-    end loop;
 
-    dbms_output.put_line('Añadido correctamente.');
+        i := pk1.tipo1.next(i);
+    end loop;
 end;
 
 update emple set salario = 160000 where emp_no = 7902;
 
 -- Ejercicio 3
 
+create or replace package pk1 as
+    type t_emple is table of emple.emp_no%type index by BINARY_INTEGER;
+    tipo1 t_emple;
+end;
+
 create or replace trigger t1
+before insert or update on emple
+begin
+    pk1.tipo1.delete;
+end;
+
+create or replace trigger t2
+after insert or update on emple
+for each row
+begin
+    pk1.tipo1(:new.emp_no) := :new.dir;
+end;
+
+create or replace trigger t3
 after insert or update on emple
 declare
     vtot number;
-    cursor c1 is select dir from emple group by dir;
+    i number;
 begin
+    i := pk1.tipo1.first;
 
-    for v1 in c1 loop
+    while i is not null loop
         select count(*) into vtot
         from emple
-        where dir = v1.dir;
+        where dir = pk1.tipo1(i);
 
         if vtot > 5 then
-            raise_application_error(-20003, 'Un empleado no puede ser jefe de más de 5 empleados.');
+            raise_application_error(-20003, 'Un jefe no puede tener más de 5 empleados a su cargo');
         end if;
+
+        i := pk1.tipo1.next(i);
     end loop;
-
-    dbms_output.put_line('Proceso terminado.');
-
 end;
 
 update emple set dir = 7698 where emp_no = 7902;
 
 -- Ejercicio 4
 
-create or replace trigger t1
+create or replace package pk1 as
+    type t_emple is table of emple.emp_no%type index by BINARY_INTEGER;
+    tipo1 t_emple;
+end;
+
+create or replace trigger t1 
+before insert or update on emple
+begin
+    pk1.tipo1.delete;
+end;
+
+create or replace trigger t2
+after insert or update on emple
+for each row
+begin
+    pk1.tipo1(:new.emp_no) := :new.dir;
+end;
+
+create or replace trigger t3
 after insert or update on emple
 declare
+    i number;
     vsal number;
-    cursor c1 is select emp_no, salario, dir
-                from emple;
+    vdirsal number;
 begin
-    for v1 in c1 loop
-        select salario into vsal 
-        from emple
-        where emp_no = v1.dir;
+    i := pk1.tipo1.first;
 
-        if v1.salario > vsal then
-            raise_application_error(-20004, 'El salario no puede ser mayor que el del jefe.');
+    while i is not null loop
+
+        select salario into vsal
+        from emple
+        where emp_no = i;
+
+        select salario into vdirsal
+        from emple
+        where emp_no = pk1.tipo1(i);
+
+        if vsal > vdirsal then
+            raise_application_error(-20004, 'El empleado no puede tener un salario mayor que el jefe.');
         end if;
+
+        i := pk1.tipo1.next(i);
     end loop;
+    
 end;
 
 update emple set salario = 400000 where emp_no = 7902;
 
 -- Ejercicio 5
 
+create or replace package pk1 as
+    type t_emple is table of emple.emp_no%type index by BINARY_INTEGER;
+    tipo1 t_emple;
+end;
+
 create or replace trigger t1
+before insert or update on emple
+begin
+    pk1.tipo1.delete;
+end;
+
+create or replace trigger t2
+after insert or update on emple
+for each row
+begin
+    pk1.tipo1(:new.emp_no) := :new.dir;
+end;
+
+create or replace trigger t3
 after insert or update on emple
 declare
     vdept number;
-    vjefe number;
-    cursor c1 is select *
-                 from emple;
+    vdeptdir number;
+    i number;
 begin
-    for v1 in c1 loop
-        select dept_no into vjefe
-        from emple
-        where emp_no = v1.dir;
-    
+
+    i := pk1.tipo1.first;
+
+    while i is not null loop
         select dept_no into vdept
         from emple
-        where emp_no = v1.emp_no;
+        where emp_no = i;
 
-        if vdept <> vjefe then
-            raise_application_error(-20004, 'El empleado no puede tener un departamento diferente al jefe.');
+        select dept_no into vdeptdir
+        from emple
+        where emp_no = pk1.tipo1(i);
+
+        if vdept <> vdeptdir then
+            raise_application_error(-20005, 'El empleado y el jefe no pueden tener un departamento diferente.');
         end if;
 
+        i := pk1.tipo1.next(i);
     end loop;
 
-    dbms_output.put_line('Tabla actualizada.');
 end;
 
 update emple set dept_no = 30 where emp_no = 7902;
