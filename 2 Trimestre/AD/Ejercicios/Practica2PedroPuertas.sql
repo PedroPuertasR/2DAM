f230d6c75194aa6987c17819e5a916c230967a42
alumno <alumno@pc10.iris.majuelo.es>
2023-02-24 21:20:33 +0100

    Ejercicio de AD actualizado

--- 2 Trimestre/AD/Ejercicios/Practica2PedroPuertas.sql
+++ 2 Trimestre/AD/Ejercicios/Practica2PedroPuertas.sql
@@ -18,6 +18,13 @@ drop table editorial;
 drop table categoria;
 drop table libro;
 
+drop type t_tienda;
+drop type t_trabajador;
+drop type t_editorial;
+drop type t_categoria;
+drop type t_libro;
+
+
 -- Creación de los objetos
 
 create or replace type t_tienda as object(
@@ -60,8 +67,30 @@ create or replace type t_libro as object(
     fecha_pub DATE,
     precio NUMBER,
     pcat ref t_categoria, 
-    ptienda ref t_tienda
+    ptienda ref t_tienda,
+    member function devolver_precio_noiva return number,
+    member procedure modi_precio(porc number)
 );
+
+create or replace type body t_libro as
+
+      member function devolver_precio_noiva
+      return number
+      is
+            iva number := 21;
+            precio_antes number := 0;
+      begin
+            precio_antes := self.precio - (self.precio * iva / 100);
+            return precio_antes;
+      end devolver_precio_noiva;
+
+      member procedure modi_precio(porc number)
+      is
+      begin
+            self.precio = self.precio * porc / 100;
+      end;
+
+end;
 
 -- Creación de las tablas
 
@@ -77,47 +106,47 @@ create table libro of t_libro;
 
 -- Inserts
 
-insert into tienda values (1, 'calle el greco 50', 10000.5);
-insert into tienda values (2, 'avenida luis montoto 25', 8000.0);
-insert into tienda values (3, 'calle alas 12', 4500.0);
+insert into tienda values (1, 'Calle El Greco 50', 10000.5);
+insert into tienda values (2, 'Avenida Luis Montoto 25', 8000.0);
+insert into tienda values (3, 'Calle Alas 12', 4500.0);
 
 
 
-insert into trabajador select 1, 'alberto', 'lopez rodriguez', '42738623t', '16/06/2014', 2500, null, ref(t), 'alloro', 'alber2014', '1001.jpg'
+insert into trabajador select 1, 'Alberto', 'López Rodríguez', '42738623t', '16/06/2014', 2500, null, ref(t), 'alloro', 'alber2014', '1001.jpg'
                        from tienda t
                        where t.id = 1;
 
-insert into trabajador select 2, 'diego', 'sánchez garcia', '20676447e', '10/09/2019', 1400, ref(tr), ref(t), 'disaga', 'diego2019', '1002.jpg'
+insert into trabajador select 2, 'Diego', 'Sánchez García', '20676447e', '10/09/2019', 1400, ref(tr), ref(t), 'disaga', 'diego2019', '1002.jpg'
                        from tienda t, trabajador tr
                        where tr.id = 1
                              and t.id = 1;
 
-insert into trabajador select 3, 'roberto', 'rosado aguilar', '12406219l', '11/12/2021', 2500, ref(tr), ref(t), 'roroag', 'rober2021', '1004.jpg'
+insert into trabajador select 3, 'Roberto', 'Rosado Aguilar', '12406219l', '11/12/2021', 2500, ref(tr), ref(t), 'roroag', 'rober2021', '1004.jpg'
                        from tienda t, trabajador tr
                        where tr.id = 1
                              and t.id = 2;
 
-insert into trabajador select 4, 'maría', 'rubio lopez', '54122047b', '25/02/2018', 1600, ref(tr), ref(t), 'marulo', 'maria2018', '1005.jpg'
+insert into trabajador select 4, 'María', 'Rubio López', '54122047b', '25/02/2018', 1600, ref(tr), ref(t), 'marulo', 'maria2018', '1005.jpg'
                        from tienda t, trabajador tr
                        where tr.id = 1
                              and t.id = 3;
 
-insert into trabajador select 5, 'alba', 'otero blanco', '67134983f', '01/02/2021', 1500, ref(tr), ref(t), 'alotbla', 'alba2021', '1007.jpg'
+insert into trabajador select 5, 'Alba', 'Otero Blanco', '67134983f', '01/02/2021', 1500, ref(tr), ref(t), 'alotbla', 'alba2021', '1007.jpg'
                        from tienda t, trabajador tr
                        where tr.id = 4
                              and t.id = 3;
 
-insert into trabajador select 6, 'sergio', 'perez crespo', '49778605g', '29/01/2022', 1000, ref(tr), ref(t), 'sepecre', 'sergi2022', '1008.jpg'
+insert into trabajador select 6, 'Sergio', 'Pérez Crespo', '49778605g', '29/01/2022', 1000, ref(tr), ref(t), 'sepecre', 'sergi2022', '1008.jpg'
                        from tienda t, trabajador tr
                        where tr.id = 2
                              and t.id = 1;
 
-insert into trabajador select 7, 'clara', 'alarcon garcia', '54391703s', '01/06/2020', 1550, ref(tr), ref(t), 'clalgar', 'clara2020', '1009.jpg'
+insert into trabajador select 7, 'Clara', 'Alarcón García', '54391703s', '01/06/2020', 1550, ref(tr), ref(t), 'clalgar', 'clara2020', '1009.jpg'
                        from tienda t, trabajador tr
                        where tr.id = 3
                              and t.id = 2;
 
-insert into trabajador select 8, 'andres', 'pelayo rodriguez', '56324370s', '21/12/2019', 1350, ref(tr), ref(t), 'anpero', 'andre2019', '1012.jpg'
+insert into trabajador select 8, 'Andrés', 'Pelayo Rodríguez', '56324370s', '21/12/2019', 1350, ref(tr), ref(t), 'anpero', 'andre2019', '1012.jpg'
                        from tienda t, trabajador tr
                        where tr.id = 3
                              and t.id = 2;
@@ -129,9 +158,9 @@ insert into editorial values(3, 'Editorial Planeta SA', 'A08186249');
 insert into editorial values(4, 'Alfaguara Grupo Editorial SL', 'B87008595');
 
 
-insert into categoria values(1, 'Fantasia');
-insert into categoria values(2, 'Ciencia ficcion');
-insert into categoria values(3, 'Comic');
+insert into categoria values(1, 'Fantasía');
+insert into categoria values(2, 'Ciencia ficción');
+insert into categoria values(3, 'Cómic');
 insert into categoria values(4, 'Suspense');
 insert into categoria values(5, 'Provisional');
 
@@ -142,7 +171,7 @@ insert into libro select 1, 'Joe Abercrombie', 'Tierras Rojas', ref(e), '9788491
                         and c.id = 2
                         and t.id = 1;
 
-insert into libro select 2, 'Kameron Hurley', 'Las estrellas son legion', ref(e), '9788491048411', '23/11/2017', 18.95, ref(c), ref(t)
+insert into libro select 2, 'Kameron Hurley', 'Las estrellas son legión', ref(e), '9788491048411', '23/11/2017', 18.95, ref(c), ref(t)
                   from tienda t, editorial e, categoria c
                   where e.id = 1
                         and c.id = 2
@@ -184,7 +213,112 @@ insert into libro select 8, 'Javier Marias', 'Los dominios del lobo', ref(e), '9
                         and c.id = 4
                         and t.id = 2;
 
+insert into libro select 9, 'Brandon Sanderson', 'El pozo de la ascensión', ref(e), '9788466658904','21/09/2016', 16.5, ref(c), ref(t)
+                  from tienda t, editorial e, categoria c
+                  where e.id = 2
+                        and c.id = 1
+                        and t.id = 2;
+
 
 select nombre, l.pedi.nombre, l.pcat.nombre, l.ptienda.direccion
 from libro l
 where id = 5;
+
+-- Creación de paquete
+
+create or replace package pk_libro as
+      procedure alta(vid integer, vautor varchar2, vnombre varchar2, visbn varchar2, vprecio number, vcat varchar2, vedi varchar2);
+      procedure baja(vtienda integer);
+      procedure modi_cat(vcat varchar2, vlibro varchar2);
+      procedure modi_edi(vedi varchar2, vlibro varchar2);
+      procedure dia_sin_iva(vtie varchar2);
+      procedure consulta_autor(vautor varchar2);
+      procedure consulta_tienda(vtienda varchar2);
+end pk_libro;
+
+create or replace package body pk_libro as
+      procedure alta(vid integer, vautor varchar2, vnombre varchar2, visbn varchar2, vprecio number, vcat varchar2, vedi varchar2) is
+      begin
+
+      end;
+
+      procedure baja(vtienda integer) is
+            cursor c1(tie integer) is select *
+                                      from libro
+                                      where ptienda.id = tie;
+            contador number := 0;
+      begin
+
+            for v1 in c1(vtienda) loop
+                  delete from libro where id = v1.id;
+                  contador := contador + 1;
+            end loop;
+
+            if contador = 0 then
+                  raise_application_error(-20002, 'El número de tienda no existe, o no existe ninguna fila con esa tienda.');
+            else
+                  dbms_output.put_line('Se han borrado ' || contador || ' filas.');
+            end if;
+
+      end;
+
+      procedure modi_cat(vcat varchar2, vlibro varchar2) is
+            vid categoria.id%type;
+            vid_libro libro.id%type;
+      begin
+            select id into vid
+            from categoria
+            where nombre = vcat;
+
+            select 
+
+
+      end;
+
+      procedure modi_edi(vedi varchar2, vlibro varchar2) is
+      begin
+            select 
+      end;
+
+      procedure dia_sin_iva (vdir varchar2) is
+            cursor c1(dir varchar2) is select * 
+                         from libro
+                         where ptienda.id = (select id
+                                             from tienda
+                                             where direccion = dir);
+            vlibro t_libro;
+      begin
+            for v1 in c1(vdir) loop
+                  if v1.precio > 15 then
+                        select deref()
+                  end if;
+            end loop;
+      end;
+
+      procedure consulta_autor(vautor varchar2) is
+            cursor c1 is select *
+      begin
+
+      end;
+
+      procedure consulta_tienda(vtienda varchar2) is
+            vid tienda.id%type;
+      begin
+            select id into vid
+            from tienda
+            where direccion = vtienda;
+
+
+      end;
+end pk_libro;
+
+
+declare
+  vtrab t_trabajador;
+begin
+  select ref(t) into vtrab
+  from trabajador t
+  where id = 1;
+  
+  dbms_output.put_line('Nombre: ' || DEREF(vtrab).nombre);
+end;
