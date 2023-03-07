0b23dcf111e6b3beca1ef8d21a1db13e57273012
alumno <alumno@pc10.iris.majuelo.es>
2023-03-07 17:24:54 +0100

    PSP terminado y empezada HLC5

--- 2 Trimestre/PSP/EnvioFicherosPedroPuertas/src/ejercicio/ClienteFichero.java
+++ 2 Trimestre/PSP/EnvioFicherosPedroPuertas/src/ejercicio/ClienteFichero.java
@@ -18,54 +18,69 @@ public class ClienteFichero {
 
     public static void main(String[] args) {
 
-        ClienteFichero objClienteFichero = new ClienteFichero();
-        objClienteFichero.pide("D:/Trabajo/hola.txt", "localhost", 6000);
+        ClienteFichero ficheroCliente = new ClienteFichero();
+        ficheroCliente.pide("/home/alumno/Documentos/hola.txt", "localhost", 6000);
     }
 
+    /*Este método pide la ruta del fichero, el servidor y el puerto de este.
+    * Con todo esto crearemos un FileOutputStream donde instanciaremos la copia
+    * del fichero y mientras recibamos datos del fichero original seguiremos
+    * copiando el archivo
+    */
     public void pide(String path, String servidor, int puerto) {
-        int numeroBytes = 0;
+        int numBytes = 0;
         try {
 
-            Socket socketCliente = new Socket(servidor, puerto);
-            ObjectOutputStream flujoSalida = new ObjectOutputStream(socketCliente.getOutputStream());
+            Socket cliente = new Socket(servidor, puerto);
+            ObjectOutputStream salida = new ObjectOutputStream(cliente.getOutputStream());
+
             MensajeDameFichero mensaje = new MensajeDameFichero();
             mensaje.nombreFichero = path;
-            System.out.println("Fichero solicitado por el cliente " + mensaje.nombreFichero);
+            System.out.println("Fichero solicitado por el cliente: " + mensaje.nombreFichero);
 
             System.out.println("********************************************************");
-            flujoSalida.writeObject(mensaje);
-            FileOutputStream ficheroCopia = new FileOutputStream(mensaje.nombreFichero
+            salida.writeObject(mensaje);
+            FileOutputStream copia = new FileOutputStream(mensaje.nombreFichero
                     + "_copia");
 
-            ObjectInputStream flujoEntrada = new ObjectInputStream(socketCliente.getInputStream());
+            ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
             MensajeTomaFichero mensajeRecibido;
-            Object mensajeAux;
+            
+            Object aux;
+            //Mientras no sea el último mensaje estaremos en el do while
             do {
-                mensajeAux = flujoEntrada.readObject();
+                aux = entrada.readObject();
 
-                if (mensajeAux instanceof MensajeTomaFichero) {
+                /*Si al leer el objeto de entrada es una instancia de 
+                * MensajeTomaFichero realiza lo siguiente:
+                 */
+                if (aux instanceof MensajeTomaFichero) {
 
-                    mensajeRecibido = (MensajeTomaFichero) mensajeAux;
+                    //Instanciamos mensajeRecibido con nuestro objecto auxiliar
+                    mensajeRecibido = (MensajeTomaFichero) aux;
 
+                    //Mostramos el texto que hay dentro del fichero
                     System.out.print(new String(mensajeRecibido.contenidoFichero, 0,
                             mensajeRecibido.bytesValidos));
-                    ficheroCopia.write(mensajeRecibido.contenidoFichero, 0,
+                    copia.write(mensajeRecibido.contenidoFichero, 0,
                             mensajeRecibido.bytesValidos);
-                    numeroBytes = numeroBytes + mensajeRecibido.bytesValidos;
+                    numBytes = numBytes + mensajeRecibido.bytesValidos;
                 } else {
 
                     System.err.println("Mensaje no esperado "
-                            + mensajeAux.getClass().getName());
+                            + aux.getClass().getName());
                     break;
                 }
             } while (!mensajeRecibido.ultimoMensaje);
             System.out.println();
             System.out.println();
-            System.out.println("Fichero copiado en " + mensaje.nombreFichero + "_copia"
-                    + "Se han copiado un total de " + numeroBytes + " Bytes");
-            ficheroCopia.close();
-            flujoEntrada.close();
-            socketCliente.close();
+            System.out.println("Fichero copiado en " + mensaje.nombreFichero + "_copia\n"
+                    + "Total de bytes copiados: " + numBytes);
+            
+            //Cerramos flujos y sockets
+            copia.close();
+            entrada.close();
+            cliente.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
